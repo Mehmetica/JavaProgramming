aee1f2038987486a1e06f3cd6ec77d68aed9e4b2
Mehmet Gezer <mehmetica74@gmail.com>
2022-11-28 10:04:30 +0300

    Inheritance Animal Task

---
+++ src/day_36_Inheritance_Intro/AnimalTask/Cat.java
@@ -0,0 +1,14 @@
+package day_36_Inheritance_Intro.AnimalTask;
+
+public class Cat extends Animal {
+
+    public void scratch(){
+        System.out.println(name+" is scrathing");
+    }
+
+    public void meow(){
+        System.out.println(name+" is meowing");
+    }
+    
+
+}
---
+++ src/day_36_Inheritance_Intro/AnimalTask/Dog.java
@@ -0,0 +1,8 @@
+package day_36_Inheritance_Intro.AnimalTask;
+
+public class Dog extends Animal {
+    public void bark(){
+        System.out.println(name+" is barking");
+    }
+
+}
